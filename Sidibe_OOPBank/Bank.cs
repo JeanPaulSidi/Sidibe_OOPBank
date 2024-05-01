eb8a26366054d2c340b6a1a79c6b67b819d616c2
JeanPaulSidi <paulsidi@outlook.com>
2024-04-30 21:51:59 -0700

    Add project files.

---
+++ Sidibe_OOPBank/Bank.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sidibe_OOPBank
+{
+    public class Bank
+    {
+        private decimal _balance;
+
+        public Bank(decimal Balance) 
+        {
+            _balance = Balance;
+        }
+
+        public decimal Balance
+        {
+            get { return _balance; }  
+        }
+
+        public string CheckBalance() 
+        {
+            string Message;
+
+            Message = ("The balance in your account is : " + _balance.ToString("C"));
+
+            return Message;
+        }
+
+        public string MakeDeposit(decimal Amount) 
+        {
+            string Message;
+
+            Message = ("The deposit of " + Amount.ToString("C") + " has been made succesfully. ");
+            _balance += Amount;
+            Message += ("The new balance in your account is: " + _balance.ToString("C"));
+
+            return Message;
+        }
+
+        private string ValidateWithdrawal(decimal Amount) 
+        {
+            string Message;
+
+            if (_balance >= Amount) 
+            {
+                Message = ("The withdrawal of " + Amount.ToString("C") + " has been accepted. ");
+                _balance -= Amount;
+                Message += ("The new balance in your account is: " + _balance.ToString("C"));
+            }
+            else 
+            {
+                Message = ("Sorry. The balance in your account is insufficient.");
+            }
+
+            return Message;
+        }
+
+        public string MakeWithdrawal(decimal Amount) 
+        {
+            string Message;
+
+            if (Amount > 500) 
+            {
+                Amount = 500;
+            }
+
+            Message = ValidateWithdrawal(Amount);
+
+            return Message;
+        }
+
+    }
+}
---
+++ Sidibe_OOPBank/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace Sidibe_OOPBank
+{
+    internal class Program
+    {
+        static ConsoleKeyInfo DisplayMenu() 
+        {
+            ConsoleKeyInfo UserChoice;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome back! What would you like to do?");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("      (B)alance          (D)eposit          (W)ithdraw          (Q)uit" );
+
+                UserChoice = Console.ReadKey(true);
+
+            } while ( UserChoice.Key != ConsoleKey.B && UserChoice.Key != ConsoleKey.D && UserChoice.Key != ConsoleKey.W && UserChoice.Key != ConsoleKey.Q );
+
+            return UserChoice;
+        }
+
+        static void ReturnToMenu() 
+        {
+            ConsoleKeyInfo UserChoice;
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to return to the menu.");
+
+            do
+            {
+                UserChoice = Console.ReadKey(true);
+
+            } while (UserChoice.Key != ConsoleKey.Enter);
+
+            Console.Clear();
+        }
+
+        static void Reconnect() 
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key if you wish to reconnect.");
+            Console.ReadKey(true);
+        }
+
+        static void Main(string[] args)
+        {
+            Bank UserAccount;
+            ConsoleKeyInfo UserChoice;
+            string Message;
+            string UserInput;
+            decimal Amount;
+
+            UserAccount = new Bank(1000M);
+
+            do 
+            {
+                UserChoice = DisplayMenu();
+
+                if (UserChoice.Key == ConsoleKey.B) 
+                {
+                    Console.Clear();
+                    Message = UserAccount.CheckBalance();
+                    Console.WriteLine(Message);
+
+                    ReturnToMenu();
+
+                }
+                else if (UserChoice.Key == ConsoleKey.D) 
+                {
+                    Console.Clear();
+                    Console.Write("Enter the amount you would like to deposit: ");
+                    UserInput = Console.ReadLine();
+                    Amount = decimal.Parse(UserInput);
+                    Console.WriteLine();
+                    Message = UserAccount.MakeDeposit(Amount);
+                    Console.WriteLine(Message);
+
+                    ReturnToMenu();
+
+                }
+                else if (UserChoice.Key == ConsoleKey.W) 
+                {
+                    Console.Clear();
+
+                    if (UserAccount.Balance == 0) 
+                    {
+                        Console.WriteLine("Sorry. Withdrawals are not allowed at this time.");
+                        Console.WriteLine();
+                    }
+                    else 
+                    {
+                        Console.Write("Enter the amount you would like to withdraw: ");
+                        UserInput = Console.ReadLine();
+                        Amount = decimal.Parse(UserInput);
+                        Console.WriteLine();
+                        Message = UserAccount.MakeWithdrawal(Amount);
+                        Console.WriteLine(Message);
+                    }
+
+                    ReturnToMenu();
+
+                }
+                else if (UserChoice.Key == ConsoleKey.Q) 
+                {
+                    Console.Clear();
+                    Console.WriteLine("Have a wonderful day, and thank you for choosing our bank.");
+
+                    Reconnect();
+
+                }
+
+            }while (true);
+        }
+    }
+}
